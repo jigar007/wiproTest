c2eb5d4e2c8fe6fc6693ff0c492b17d488a27de3
thakkarj <thakkarj@student.unimelb.edu.au>
2018-02-18 17:39:13 +1100

    Initial Commit

---
+++ wiproTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  wiproTest
+//
+//  Created by Jigar Thakkar on 17/2/18.
+//  Copyright © 2018 Jigar Thakkar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ wiproTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wiproTest
+//
+//  Created by Jigar Thakkar on 17/2/18.
+//  Copyright © 2018 Jigar Thakkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
